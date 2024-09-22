d659b2da87f307b60cc432baaa7ae97572dd8701
dinglevin <dingweiqiong@gmail.com>
2024-09-23 00:37:08 +0800

    添加hint组合支持

--- honkit-plugin-tooltips/index.js
+++ honkit-plugin-tooltips/index.js
@@ -1,3 +1,27 @@
+const ALERT_STYLES = {
+    info:'info',
+    tip:'success',
+    danger:'danger',
+    working:'warning'
+};
+
+const ALLERT_STYLES_CLASS = {
+    info: "fa fa-info-circle",
+    tip: "fa fa-mortar-board",
+    danger: "fa fa-exclamation-circle",
+    working: "fa fa-wrench"
+}
+
+function wrapIfHint(body, hintStyle) {
+    if (hintStyle) {
+        return `<div class="alert alert-${ALERT_STYLES[hintStyle]} hints-alert">
+            <div class="hints-icon"><i class="${ALLERT_STYLES_CLASS[hintStyle]}"></i></div>
+            <div class="hints-container">${body}</div>
+        </div>`
+    }
+    return body;
+}
+
 function doSplit(body, delimiter) {
     const list = body.split(delimiter);
     if (list.length === 0) {
@@ -21,15 +45,22 @@ function doSplit(body, delimiter) {
 module.exports = {
     blocks: {
         tooltips: {
-            process: function (block) {
+            process: async function (block) {
                 const pluginConfig = this.config.get('pluginsConfig.tooltips');
                 const delimiter = block.kwargs?.delimiter || pluginConfig?.delimiter || "@@";
                 const direction = block.kwargs?.direction || "bottom";
+                const hintStyle = block.kwargs?.hintStyle;
                 const { body, tooltipstext } = doSplit(block.body.trim(), delimiter);
+
+                const renderedBody = await this.book.renderBlock("markdown", body);
+                const rendredTooltipsText = await this.book.renderBlock("markdown", tooltipstext);
+
+                const wrappedBody = wrapIfHint(renderedBody, hintStyle);
+
                 return `
                     <div class="tooltips">
-                        ${body}
-                        <span class="tooltipstext-${direction}">${tooltipstext}</span>
+                        ${wrappedBody}
+                        <div class="tooltipstext-${direction}">${rendredTooltipsText}</div>
                     </div>
                 `
             }
